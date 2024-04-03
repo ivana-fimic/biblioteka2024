c912f1e525bf97eb751a9183604452e8a66d637e
Ivana <Ivana@Vivobook>
2024-04-03 10:12:06 +0200

    cetvrti commit

---
+++ BibliotekaGit/src/biblioteka/interfejs/BibliotetekaIntrefejs.java
@@ -0,0 +1,13 @@
+package biblioteka.interfejs;
+
+import java.util.List;
+
+import biblioteka.Autor;
+import biblioteka.Knjiga;
+
+public interface BibliotetekaIntrefejs {
+	public void dodajKnjigu(Knjiga knjiga);
+	public void obrisiKnjigu(Knjiga knjiga);
+	public List<Knjiga> VratiSveKnjige();
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov,String izdavac);
+}
